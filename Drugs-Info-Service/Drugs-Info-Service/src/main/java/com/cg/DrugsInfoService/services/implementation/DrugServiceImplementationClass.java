1dc1ffd90a127d416cca2c681d9520d209fe5190
Surya0704 <47137297+Surya0704@users.noreply.github.com>
2022-08-22 16:52:32 +0530

    Service Implememtation Class Created with Update not included

---
+++ Drugs-Info-Service/Drugs-Info-Service/src/main/java/com/cg/DrugsInfoService/services/implementation/DrugServiceImplementationClass.java
@@ -0,0 +1,36 @@
+package com.cg.DrugsInfoService.services.implementation;
+
+import com.cg.DrugsInfoService.models.DrugsData;
+import com.cg.DrugsInfoService.repository.DrugsRepository;
+import com.cg.DrugsInfoService.services.DrugService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class DrugServiceImplementationClass implements DrugService {
+
+    @Autowired
+    DrugsRepository drugsRepository;
+
+    @Override
+    public DrugsData saveDrugsData(DrugsData drugsData) {
+        return drugsRepository.save(drugsData);
+    }
+
+    @Override
+    public List<DrugsData> getAllDrugsData() {
+        return drugsRepository.findAll();
+    }
+
+    @Override
+    public Optional<DrugsData> findDrugsById(String drugId) {
+        return drugsRepository.findById(drugId);
+    }
+
+    @Override
+    public void deleteDrugsData(String drugId) {
+        drugsRepository.deleteById(drugId);
+
+    }
+}
