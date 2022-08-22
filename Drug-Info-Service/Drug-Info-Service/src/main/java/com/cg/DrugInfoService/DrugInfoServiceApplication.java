6628ed61badabc5a3b90edcd2c5f0b3d82adc707
Surya0704 <47137297+Surya0704@users.noreply.github.com>
2022-08-22 16:17:42 +0530

    Init Commit

---
+++ Drug-Info-Service/Drug-Info-Service/src/main/java/com/cg/DrugInfoService/DrugInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cg.DrugInfoService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugInfoServiceApplication.class, args);
+	}
+
+}
---
+++ Drug-Info-Service/Drug-Info-Service/src/test/java/com/cg/DrugInfoService/DrugInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.DrugInfoService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
